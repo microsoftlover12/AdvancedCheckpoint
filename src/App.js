ce99eabd965e95e537286303dc04e55222cdda83
LABBASSIBassem <49342568+LABBASSIBassem@users.noreply.github.com>
2020-06-11 12:09:42 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,64 +7,23 @@ import PropTypes from 'prop-types';
 
 function App(Props) {
 
-  return productsTable.map((category, name, price) => {
+  return
+  (<table>
+   {productsTable.map((category, name, price) => {
 
     return (
-      <table>
         <tr>
-
           <td>{Props.category}</td>
           <td>{Props.name}</td>
           <td>{Props.price}</td>
-
         </tr>
-        <tr>
-
-          <td>{Props.category}</td>
-          <td>{Props.name}</td>
-          <td>{Props.price}</td>
-
-        </tr>
-
-        <tr>
-
-          <td>{Props.category}</td>
-          <td>{Props.name}</td>
-          <td>{Props.price}</td>
-
-        </tr>
-
-
+    )}
       </table>
-
-    )
   })
 
-
-
-
-
-
 }
-
-
-class MyStyle extends React.Component{
-
-
-  render(){
-
-  return <h1 className={styles.bigblue}>Welcome to my first props page! </h1>
-}
-
-}
-export default MyStyle;
-
-
-
 
 ReactComponent.PropTypes={
-
-
   category,name:PropTypes.string,
   price:PropTypes.number
 
